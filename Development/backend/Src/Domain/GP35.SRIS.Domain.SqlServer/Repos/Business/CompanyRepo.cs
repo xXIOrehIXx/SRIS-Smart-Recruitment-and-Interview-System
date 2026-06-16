c722067cf759330c9f6faec8656a3f29a86b2fe1
jdodlsuw <jdodlsuw@gmail.com>
2026-06-16 14:10:35 +0700

    fix: GetByCompanyId

--- Development/backend/Src/Domain/GP35.SRIS.Domain.SqlServer/Repos/Business/CompanyRepo.cs
+++ Development/backend/Src/Domain/GP35.SRIS.Domain.SqlServer/Repos/Business/CompanyRepo.cs
@@ -1,34 +1,23 @@
 using Dapper;
+
 using GP35.SRIS.Domain.Connection;
 using GP35.SRIS.Domain.Entities;
 using GP35.SRIS.Domain.Repos;
+
 using Microsoft.Extensions.DependencyInjection;
 
 namespace GP35.SRIS.Domain.SqlServer.Repos;
 
 public class CompanyRepo : BaseRepo<long, Company>, ICompanyRepo
 {
-  protected IConnectionManager _connectionManager;
+	public CompanyRepo(IServiceProvider serviceProvider) : base(serviceProvider)
+	{
+	}
 
-  public CompanyRepo(IServiceProvider serviceProvider) : base(serviceProvider)
-  {
-    _connectionManager = serviceProvider.GetRequiredService<IConnectionManager>();
-  }
+	public async Task<Company> GetByCompanyId(long companyId)
+	{
+		var cmd = $"SELECT * FROM {_tableName} WHERE company_id = @companyId";
 
-  public async Task<Company> GetByCompanyId(long companyId)
-  {
-    var connection = await _connectionManager.GetDbConnectionAsync();
-
-    connection.Execute("ALTER SECURITY POLICY [dbo].[TenantSecurityPolicy] WITH (STATE = OFF);");
-
-    var cmd = $"SELECT * FROM {_tableName} WHERE company_id = @companyId";
-    try
-    {
-      return await connection.QuerySingleOrDefaultAsync<Company>(cmd, new { companyId });
-    }
-    finally
-    {
-      connection.Execute("ALTER SECURITY POLICY [dbo].[TenantSecurityPolicy] WITH (STATE = ON);");
-    }
-  }
+		return await QuerySingleOrDefaultAsync(cmd, new { companyId });
+	}
 }
