70610f0dd17a5b7ee62537d7fa58d298fcc8fc15
sannxt <sannxt244@gmail.com>
2026-08-29 03:44:14 +0700

    fix: color logo

--- Development/frontend/src/components/LogoIcon.jsx
+++ Development/frontend/src/components/LogoIcon.jsx
@@ -4,7 +4,7 @@ import Icon from '@ant-design/icons';
 const LogoIcon = ({ size = 36, color = '#5D8C3E', className, style, ...props }) => {
   const LogoSvg = (svgProps) => (
     <svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" {...svgProps}>
-      <rect width="48" height="48" rx="12" fill={color} />
+      <rect width="48" height="48" rx="12" />
       <path
         d="M14 16C14 14.8954 14.8954 14 16 14H32C33.1046 14 34 14.8954 34 16V32C34 33.1046 33.1046 34 32 34H16C14.8954 34 14 33.1046 14 32V16Z"
         stroke="white"
@@ -29,7 +29,7 @@ const LogoIcon = ({ size = 36, color = '#5D8C3E', className, style, ...props }) 
   return (
     <Icon 
       component={LogoSvg} 
-      style={{ fontSize: size, ...style }} 
+      style={{ fontSize: size, color, ...style }} 
       className={className}
       {...props} 
     />
